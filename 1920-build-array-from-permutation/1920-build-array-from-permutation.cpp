3d59665f2b698c24b596bf76aec338c0149b1035
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-02-27 07:57:47 +0530

    Update 1920-build-array-from-permutation.cpp

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,3 +1,24 @@
+// TIME : O(N) SPACE : O(N)
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
+
+// Optimization over the O(N) space solution!
+// TIME : O(N) SPACE : O(1)
+/*
+ How?
+ - let's say a number x is changed to x = x + n * (y % n)
+ - now adding x to z => z = z + n * ((x + n * (y % n)) % n) => z + n * x
+ - later divide by n to remove the extra remainder
+*/
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
