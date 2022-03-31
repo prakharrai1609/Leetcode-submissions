4a50fdd3a2c03bd0f0b3f08249067aaf4b9d41d7
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-03-31 08:03:47 +0530

    Time: 3 ms (63.24%), Space: 6.9 MB (92.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    // BRUTE FORCE
+    
+    void rotate(vector<vector<int>>& matrix) {
+        // think about this like paper folding, how will you fold the paper to get the desired shape
+        // first fold the matrix about first row 
+        
+        int fr = 0, lr = matrix.size() - 1, N = matrix[0].size();
+        
+        while (fr < lr) {
+            for (int j = 0; j < N; j++) {
+                swap(matrix[fr][j], matrix[lr][j]);
+            }
+            fr++, lr--;
+        }
+        
+        // now matrix is flipped across the first row
+        // now we flip it along diagonal to get the desired reflection
+        
+        // top triangle        
+        int row1, col1, row2, col2;
+        for (int i = 0; i < N; i++) {
+            row1 = 0, col1 = i, row2 = i, col2 = 0;
+            while (row1 < row2) {
+                swap(matrix[row1][col1], matrix[row2][col2]);
+                row1++, row2--;
+                col1--, col2++;
+            }
+        }
+        
+        // bottom triangle
+        for (int i = 1; i < N; i++) {
+            row1 = i, col1 = N - 1, row2 = N - 1, col2 = i;
+            while (row1 < row2) {
+                swap(matrix[row1][col1], matrix[row2][col2]);
+                row1++, row2--;
+                col1--, col2++;
+            }
+        }
+    }
+    
+    // SMART METHOD
+    
     /*
      * clockwise rotate
      * first reverse up to down, then swap the symmetry 
@@ -7,7 +49,7 @@ public:
      * 4 5 6  => 4 5 6  => 8 5 2
      * 7 8 9     1 2 3     9 6 3
     */
-    void rotate(vector<vector<int> > &matrix) {
+    void rotate1(vector<vector<int> > &matrix) {
         reverse(matrix.begin(), matrix.end());
         for (int i = 0; i < matrix.size(); ++i) {
             for (int j = i + 1; j < matrix[i].size(); ++j)
