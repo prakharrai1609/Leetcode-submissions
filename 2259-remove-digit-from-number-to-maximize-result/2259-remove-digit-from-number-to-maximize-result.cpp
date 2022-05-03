dca113eefbe850a7b6a2b725045530ee4dfba4c9
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-05-03 19:06:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (92.47%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
+        int N = number.length();
+        for (int i = 0; i < N - 1; i++) {
+            if (number[i] == digit and number[i] < number[i + 1]) {
+                return (number.substr(0, i) + number.substr(i + 1));
+            }
+        }
+        
+        for (int i = N - 1; i >= 0; --i) {
+            if (number[i] == digit)
+                return number.erase(i, 1);
+        }
+        
+        return number;
+    }
+};
+
+/*
+    BAD APPROACH -- WASTAGE OF SPACE
+    ================================
         vector<string> db;
         int N = number.length();
         
@@ -13,5 +32,4 @@ public:
         
         string ans = *max_element(db.begin(), db.end());
         return ans;
-    }
-};
+*/
