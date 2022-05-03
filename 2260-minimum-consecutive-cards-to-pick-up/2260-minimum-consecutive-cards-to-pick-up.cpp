9b0466cb8afea1d25e83ebc709ea90b6872a1528
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-05-03 19:15:05 +0530

    Time: 1240 ms (7.97%), Space: 199.9 MB (11.08%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& cards) {
+        /*
+            Approach: 
+            - storing all the indices in the vector
+            - calculating the pair wise minimum
+        */
         map<int, vector<int>> m;
         for (int i = 0; i < cards.size(); i++) {
             m[cards[i]].push_back(i + 1);
