ed8feada3a268fef926be0e83ccc71b87b40e22b
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-05-03 19:24:48 +0530

    Time: 302 ms (96.64%), Space: 79.5 MB (98.69%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& cards) {
-        /*
-            Approach: 
-            - storing all the indices in the vector
-            - calculating the pair wise minimum
-        */
+        int prev[1000001] = {0}, ans = INT_MAX;
+        
+        for (int i = 0; i < cards.size(); i++) {
+            if (prev[cards[i]])
+                ans = min(ans, (i + 1) - prev[cards[i]] + 1);
+            prev[cards[i]] = i + 1;
+        }
+        
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
+    
+/*
+    Approach: 
+    - storing all the indices in the vector
+    - calculating the pair wise minimum
+
+    Improvement:
+    - instead of keeping track of all the indices
+    just keep track of previous value
+
+    CODE
+    ====
         unordered_map<int, vector<int>> m;
         for (int i = 0; i < cards.size(); i++) {
             m[cards[i]].push_back(i + 1);
         }
-        
+
         int ans = INT_MAX;
         for (auto x : m) {
             if (x.second.size() > 1) {
@@ -20,7 +38,7 @@ public:
                 }
             }
         }
-        
+
         return (ans == INT_MAX ? -1 : ans);
-    }
-};
+        }
+*/
