e89be763f584db1604c83452bc2b9374a8a24f76
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-05-10 09:59:04 +0530

    Time: 3 ms (45.41%), Space: 6.6 MB (42.22%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,4 +1,54 @@
 class Solution {
+private:
+    vector<vector<int>> ans;
+public:
+    /*
+        BACKTRACKING BASED SOLUTION:
+        ===========================        
+    */
+    
+    
+    int sum(const vector<int>& combo) {
+        int temp = 0;
+        for (int x : combo)
+            temp += x;
+        return temp;
+    }
+    
+    void helper(vector<int>& combo, int k, int n, int num) {
+        if (!k) {
+            if (sum(combo) == n)
+                ans.push_back(combo);
+            return;
+        }
+        
+        if (num > 9) return;
+        
+        combo.push_back(num);
+        helper(combo, k - 1, n, num + 1);
+        combo.pop_back();
+        helper(combo, k, n, num + 1);
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> combo;
+        helper(combo, k, n, 1);
+        return ans;
+    }
+};
+
+    
+/*
+    BRUTE FORCE | BIT MANIPULATION
+    ==============================
+
+
+	This solution involves checking all possible combinations possible :
+	- "int bits = __builtin_popcount(i);" here we check if their are K numbers or not
+	- if there are K numbers present, then we just check the sum of all 
+
+
+class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
@@ -22,3 +72,5 @@ public:
         return ans;
     }
 };
+
+*/
