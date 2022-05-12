8fcdc98f9ebc88309233da57904ea563fdfe2cbf
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-05-12 06:36:31 +0530

    Time: 3 ms (45.24%), Space: 5.8 MB (85.71%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,6 +1,27 @@
 class Solution {
+    int a = 1, e = 1, i = 1, o = 1, u = 1;
 public:
-    int countVowelStrings(int n) {              
-        return ((n+1)*(n+2)*(n+3)*(n+4))/24;
+    void round() {
+        a += e + i + o + u;
+        e += i + o + u;
+        i += o + u;
+        o += u;
+    }
+    
+    int countVowelStrings(int n) {
+        n -= 1;
+        while (n--)
+            round();
+        return a + e + i + o + u;
     }
 };
+
+/*
+    MATH BASED APPROACH
+    -------------------
+    
+    - we know that if we have some sets (1 to 4) of (a, e, i, o, u), a will be incremented as :
+        # (a, e, i, o, u) (a, e, i, o, u) (a, e, i, o, u)
+        # [[aa ae ai ao au] [ee, ei, eo, eu], .. [uu]] (a, e, i, o, u)
+        # and so on, we can understand that easily
+*/
