d376a07e8c727e5871a8803a3ac8cb6cecfc58a5
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-07-11 14:58:14 +0530

    Time: 3 ms (86.54%), Space: 13.7 MB (48.59%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,41 +1,61 @@
 class Solution {
 public:
-    bool check(string brackets) {
-        stack<char> s;
-        for (char bracket : brackets) {
-            if (s.empty() and bracket == ')')
-                return false;
-            
-            if (s.empty() or s.top() == bracket)
-                s.push(bracket);
-            else
-                s.pop();
-        }
-        
-        return s.empty();
+    void backtrack(vector<string>& vs, string ans, int open, int closed, int max) {
+        if (ans.length() == 2 * max)
+            vs.push_back(ans);
+        if (open < max)
+            backtrack(vs, ans + '(', open + 1, closed, max);
+        if (closed < open)
+            backtrack(vs, ans + ')', open, closed + 1, max);
     }
     
     vector<string> generateParenthesis(int n) {
-        int total = 2 * n;
-        vector<string> ans;
-        
-        for (int i = 0; i < (1 << total); i++) {
-            string temp = "";
-            for (int j = 0; j < total; j++) {
-                if (i & (1 << j)) {
-                    temp = ")" + temp;
-                } else {
-                    temp = "(" + temp;
-                }
-            }
-            
-            if (check(temp))
-                ans.push_back(temp);
-        }
-        
-        return ans;
+        vector<string> vs;
+        backtrack(vs, "", 0, 0, n);
+        return vs;
     }
 };
+
+// Bit manipulation approach
+
+// class Solution {
+// public:
+//     bool check(string brackets) {
+//         stack<char> s;
+//         for (char bracket : brackets) {
+//             if (s.empty() and bracket == ')')
+//                 return false;
+            
+//             if (s.empty() or s.top() == bracket)
+//                 s.push(bracket);
+//             else
+//                 s.pop();
+//         }
+        
+//         return s.empty();
+//     }
+    
+//     vector<string> generateParenthesis(int n) {
+//         int total = 2 * n;
+//         vector<string> ans;
+        
+//         for (int i = 0; i < (1 << total); i++) {
+//             string temp = "";
+//             for (int j = 0; j < total; j++) {
+//                 if (i & (1 << j)) {
+//                     temp = ")" + temp;
+//                 } else {
+//                     temp = "(" + temp;
+//                 }
+//             }
+            
+//             if (check(temp))
+//                 ans.push_back(temp);
+//         }
+        
+//         return ans;
+//     }
+// };
 
 /*
     n == 3
