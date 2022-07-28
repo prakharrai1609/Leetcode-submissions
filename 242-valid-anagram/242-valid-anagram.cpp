e151a952581da42163923c4dc7cc2fa836371645
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-07-28 22:52:32 +0530

    Time: 8 ms (82.77%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
+        
+        int f1[26] = {0}, f2[26] = {0}, N = s.length();
+    
+        for (int i = 0; i < N; i++) {
+            f1[s[i] - 'a']++;
+            f2[t[i] - 'a']++;
+        }
+        
+        for (int i = 0; i < 26; i++)
+            if (f1[i] != f2[i])
+                return false;
+        
+        return true;
+    }
+};
+
+
+/*  TIME C : NLOGN
+    SPACE : 1
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
         sort(begin(s), end(s));
         sort(begin(t), end(t));
         int i = 0, N = s.length();
@@ -14,4 +35,5 @@ public:
         
         return true;
     }
-};
+
+**/
