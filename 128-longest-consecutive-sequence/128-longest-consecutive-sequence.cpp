c05bbe30c3d3bda3d60469f58930954877bd36fb
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-08-02 21:27:23 +0530

    Time: 182 ms (70.79%), Space: 45.6 MB (72.95%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-	if(!size(nums)) return 0;
-	sort(begin(nums), end(nums));
-	int longest = 0, cur_longest = 1;
-	for(int i = 1; i < size(nums); i++) 
-		if(nums[i] == nums[i - 1]) continue;
-		else if(nums[i] == nums[i - 1] + 1) cur_longest++; // consecutive element - update current streak length
-		else longest = max(longest, cur_longest), cur_longest = 1;  // reset current streak length
-	return max(longest, cur_longest);
-}
+        unordered_set<int> s(begin(nums), end(nums));
+        int len = 0;
+        for (int num : s) {
+            int t = 0;
+            if (s.count(num - 1)) continue;
+            else {
+                int n = num;
+                while (s.count(n)) {
+                    t++;
+                    n++;
+                }
+                len = max(t, len);
+            }
+        }
+        return len;
+    }
 };
+
+/*  TIME : NLOGN SPACE : 1
+
+    int longestConsecutive(vector<int>& nums) {
+        if(!size(nums)) return 0;
+        sort(begin(nums), end(nums));
+        int longest = 0, cur_longest = 1;
+        for(int i = 1; i < size(nums); i++) 
+            if(nums[i] == nums[i - 1]) continue;
+            else if(nums[i] == nums[i - 1] + 1) cur_longest++; 
+            else longest = max(longest, cur_longest), cur_longest = 1;
+        return max(longest, cur_longest);
+    }
+
+*/
