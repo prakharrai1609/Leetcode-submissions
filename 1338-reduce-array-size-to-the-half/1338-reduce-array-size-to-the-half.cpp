b130cca0cbb6982b941094d25fe5caa5864fe338
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-08-18 07:26:13 +0530

    Time: 311 ms (61.88%), Space: 77.8 MB (85.48%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        map<int, int> f;
-        vector<pair<int, int>> v;
-        for (int i = 0; i < arr.size(); i++)
-            f[arr[i]]++;
-        for (auto x : f)
-            v.push_back({x.second, x.first});
-        sort(v.begin(), v.end(), greater<pair<int, int>>());
-        int cnt = 0, ans = 0, N = arr.size();
-        if (N & 1) 
-            N += 1;
-        N /= 2;
-        
-        for (auto p : v) {
-            if (cnt >= N) break;
-            ans++;
-            N -= p.first;
+        int n = arr.size();
+        unordered_map<int, int> cnt;
+        for (int x : arr) ++cnt[x];
+
+        vector<int> counting(n + 1);
+        for (auto [_, freq] : cnt) ++counting[freq];
+
+        int ans = 0, removed = 0, half = n / 2, freq = n;
+        while (removed < half) {
+            ans += 1;
+            while (counting[freq] == 0) --freq;
+            removed += freq;
+            --counting[freq];
         }
-        
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int minSetSize(vector<int>& arr) {
+//         map<int, int> f;
+//         vector<pair<int, int>> v;
+//         for (int i = 0; i < arr.size(); i++)
+//             f[arr[i]]++;
+//         for (auto x : f)
+//             v.push_back({x.second, x.first});
+//         sort(v.begin(), v.end(), greater<pair<int, int>>());
+//         int cnt = 0, ans = 0, N = arr.size();
+//         N /= 2;
+        
+//         for (auto p : v) {
+//             if (cnt >= N) break;
+//             ans++;
+//             N -= p.first;
+//         }
+        
+//         return ans;
+//     }
+// };
