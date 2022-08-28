9e63b67f434b7b4502bca46e39d9558879b0579b
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-08-28 13:48:08 +0530

    Time: 93 ms (62.33%), Space: 18.8 MB (7.49%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     int maximalSquare(vector<vector<char>>& matrix) {
+//         int N = matrix.size(), M = matrix[0].size();
+//         int dp[N][M];
+//         int ans = 0;
+//         for (int i = 0; i < N; i++) {
+//             for (int j = 0; j < M; j++) {
+//                 if (!i or !j or matrix[i][j] == '0') {
+//                     dp[i][j] = matrix[i][j] - '0';
+//                 } else {
+//                     dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
+//                 }
+//                 ans = max(ans, dp[i][j]);
+//             }
+//         }
+        
+//         return ans * ans;
+//     }
+// };
+
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int N = matrix.size(), M = matrix[0].size();
-        int dp[N][M];
+        vector<int> prev(M) , curr(M);
         int ans = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (!i or !j or matrix[i][j] == '0') {
-                    dp[i][j] = matrix[i][j] - '0';
+                if (i == 0 or j == 0 or matrix[i][j] == '0') {
+                    curr[j] = matrix[i][j] - '0';
                 } else {
-                    dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
+                    curr[j] = min({curr[j - 1], prev[j], prev[j - 1]}) + 1;
                 }
-                ans = max(ans, dp[i][j]);
+                ans = max(ans, curr[j]);
             }
+            swap(curr, prev);
         }
         
         return ans * ans;
