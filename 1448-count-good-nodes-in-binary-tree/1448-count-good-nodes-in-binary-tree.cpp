b802e09f5dbb179f7d4d36c526b3450e8f854a92
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-09-01 07:46:09 +0530

    Time: 210 ms (48.52%), Space: 86.4 MB (9.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,25 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+//     multiset<int> s;
+//     int ans;
+// public:
+//     void go(TreeNode* root) {
+//         if (!root)
+//             return;
+//         s.insert(root->val);
+//         go(root->left);
+//         if (!s.empty() and root->val >= *(--s.end())) {
+//             ans++;
+//         }
+//         go(root->right);
+//         s.erase(s.lower_bound(root->val));
+//     }
+    
+//     int goodNodes(TreeNode* root) {
+//         ans = 0;
+//         go(root);
+//         return ans;
+//     }
+// };
+
 class Solution {
-    multiset<int> s;
     int ans;
 public:
-    void go(TreeNode* root) {
+    void go(TreeNode* root, int max_yet) {
         if (!root)
             return;
-        s.insert(root->val);
-        go(root->left);
-        if (!s.empty() and root->val >= *(--s.end())) {
+        int curr_max = max(max_yet, root->val);
+        
+        go(root->left, curr_max);
+        if (root->val >= max_yet) {
             ans++;
         }
-        go(root->right);
-        s.erase(s.lower_bound(root->val));
+        go(root->right, curr_max);
     }
     
     int goodNodes(TreeNode* root) {
         ans = 0;
-        go(root);
+        go(root, INT_MIN);
         return ans;
     }
 };
