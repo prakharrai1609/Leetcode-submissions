3d1375e24e0aa3ab74219d77d5dc6b79c08994e6
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2022-10-01 11:08:58 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.87%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,52 +1,26 @@
-// class Solution {
-//     vector<int> dp;
-// public:
-//     int count(string s, int i) {
-//         if (i == s.length())
-//             return 1;
-        
-//         if (s[i] == '0')
-//             return 0;
-        
-//         if (dp[i] != -1)
-//             return dp[i];
-        
-//         int op1 = 0, op2 = 0;
-        
-//         // pick 1 char
-//         op1 = count(s, i + 1);
-//         // pick 2 char
-//         if (i + 1 < s.length()) {
-//             if (s[i] == '1') {
-//                 op2 = count(s, i + 2);
-//             } else if (s[i] == '2' and s[i + 1] < '7') {
-//                 op2 = count(s, i + 2);
-//             }
-//         }
-        
-//         return dp[i] = op1 + op2;
-//     }
-    
-//     int numDecodings(string s) {
-//         dp.resize(s.length(), -1);
-//         return count(s, 0);   
-//     }
-// };
-
 class Solution {
+    vector<int> dp;
 public:
-    int numDecodings(string s) {
-        if (s[0] == '0') return 0;
-        
-        vector<int> dp(s.length() + 1);
-        dp[0] = dp[1] = 1;
-        
-        for (int i = 2; i <= s.length(); i++) {
-            if (s[i - 1] != '0') dp[i] = dp[i - 1];
-            if (s[i - 2] == '1' or (s[i - 2] == '2' and s[i - 1] < '7'))
-                dp[i] += dp[i - 2];
+    int solve(string s, int idx) {
+        if (idx >= s.length())
+            return 1;
+        int op1 = 0, op2 = 0;
+        if (s[idx] == '0')
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        op1 = solve(s, idx + 1);
+        if (idx + 1 < s.length()) {
+            int num = ((s[idx + 1] - '0') + 10 * (s[idx] - '0'));
+            if (num <= 26)
+                op2 = solve(s, idx + 2);
         }
         
-        return dp[s.length()];
+        return dp[idx] = op1 + op2;
+    }
+    
+    int numDecodings(string s) {
+        dp.resize(s.length(), -1);       
+        return solve(s, 0);
     }
 };
